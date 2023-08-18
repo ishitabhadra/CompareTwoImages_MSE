19b62361e0a55f9560705a3acd1d9fee1fc90839
ishitabhadra <114369699+ishitabhadra@users.noreply.github.com>
2023-08-18 01:34:20 -0700

    Update MSE.py

--- MSE.py
+++ MSE.py
@@ -3,12 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # load the input images
-img1 = cv2.imread('img1.png')
-img2 = cv2.imread('img2.png')
+img1 = cv2.imread('pho_6.png')
+img2 = cv2.imread('voy_6.png')
+
+img1_1 = cv2.imread('voy_6.png')
+img2_1 = cv2.imread('pho_6.png')
 
 # convert the images to grayscale
 img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
 img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+img1_1 = cv2.cvtColor(img1_1, cv2.COLOR_BGR2GRAY)
+img2_1 = cv2.cvtColor(img2_1, cv2.COLOR_BGR2GRAY)
 
 # define the function to compute MSE between two images
 def mse(img1, img2):
@@ -19,6 +24,10 @@ def mse(img1, img2):
    return mse, diff
 
 error, diff = mse(img1, img2)
+print("Image matching Error between the two images:",error)
+#plt.imshow(diff)
+
+error, diff = mse(img1_1, img2_1)
 print("Image matching Error between the two images:",error)
 
 plt.imshow(diff)
