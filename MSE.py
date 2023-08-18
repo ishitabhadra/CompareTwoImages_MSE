5596b80798f8aacb3a63a0ba8765665ba601984f
ishitabhadra <114369699+ishitabhadra@users.noreply.github.com>
2023-08-18 00:27:17 -0700

    Create MSE.py

---
+++ MSE.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# load the input images
+img1 = cv2.imread('pho_1.png')
+img2 = cv2.imread('voy_1.png')
+
+# convert the images to grayscale
+img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+# define the function to compute MSE between two images
+def mse(img1, img2):
+   h, w = img1.shape
+   diff = cv2.subtract(img1, img2)
+   err = np.sum(diff**2)
+   mse = err/(float(h*w))
+   return mse, diff
+
+error, diff = mse(img1, img2)
+print("Image matching Error between the two images:",error)
+
+plt.imshow(diff)
